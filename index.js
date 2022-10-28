6d3a9cf2227be1652262e413a99d696cb19226c2
Sheikh Riyadh <sheikhriyad350883@gmail.com>
2022-10-28 22:03:07 +0600

    added and modify course and details json files

--- index.js
+++ index.js
@@ -5,9 +5,27 @@ const port = process.env.PORT || 5000
 
 app.use(cors())
 
+const courses = require('./data/courses.json');
+const details = require("./data/details.json");
+
 
 app.get('/', (req, res) => {
-    res.send("Hello i am calling from server");
+    res.send("Hey server is running")
+})
+app.get('/courses', (req, res) => {
+    res.send(courses);
+})
+
+app.get('/courses/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const searchCourse = details.find(detail => detail.courseID === id)
+    res.send(searchCourse)
+})
+
+app.get('/details/:id', (req, res) => {
+    const id = req.params.id;
+    const searchDetails = details.find(detail => detail.id === id)
+    res.send(searchDetails)
 })
 
 app.listen(port, () => {
